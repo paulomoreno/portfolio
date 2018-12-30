dbf8e754d196feb945dee3f32a719eb87f974499
Paulo Moreno <paulomorenoo@gmail.com>
2018-12-30 11:46:37 -0200

    Create pages for bandejao and beer tracker

---
+++ src/pages/beer-tracker.jsx
@@ -0,0 +1,54 @@
+/* global tw */
+import React from 'react';
+import styled from 'react-emotion';
+import 'typeface-cantata-one';
+import 'typeface-open-sans';
+import { Parallax, ParallaxLayer } from 'react-spring/dist/addons';
+import { rotate } from '../styles/animations';
+import triangle from '../images/triangle.svg';
+ 
+import '../styles/global';
+
+const Content = styled(ParallaxLayer)`
+  ${tw('p-6 md:p-12 lg:p-24 justify-center items-center flex z-50')};
+`;
+
+const Hero = styled.div`
+  ${tw('w-full xl:w-2/3')};
+`;
+
+const Title = styled.h1`
+  ${tw('text-4xl lg:text-4xl font-sans text-white mb-8 tracking-wide relative inline-block')};
+  text-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+  &:before {
+    content: '';
+    width: 40px;
+    height: 40px;
+    background: url(${triangle});
+    position: absolute;
+    background-size: 40px;
+    animation: ${rotate} 4s linear infinite;
+    left: -60px;
+    top: 5px;
+  }
+`;
+
+const Subtitle = styled.p`
+  ${tw('text-2xl lg:text-4xl font-sans text-white mt-8 xxl:w-3/4')};
+  text-shadow: 0 2px 15px rgba(0, 0, 0, 0.2);
+`;
+
+const Index = () => (
+  <React.Fragment>
+    <Parallax pages={1}>
+      <Content speed={0.4} offset={0}>
+        <Hero>
+            <Title>Beer Tracker</Title>
+            <Subtitle>Under construction</Subtitle>
+        </Hero>
+      </Content>
+    </Parallax>
+  </React.Fragment>
+);
+
+export default Index;
