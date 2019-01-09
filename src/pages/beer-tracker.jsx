ec17fd74b4e27eb13abf2ea2423d41d2d057384e
Paulo Moreno <paulomorenoo@gmail.com>
2019-01-09 21:31:27 -0200

    WIP - Add initial text and layout

--- src/pages/beer-tracker.jsx
+++ src/pages/beer-tracker.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import styled from 'react-emotion';
 import 'typeface-cantata-one';
 import 'typeface-open-sans';
-import { Parallax, ParallaxLayer } from 'react-spring/dist/addons';
-import { rotate } from '../styles/animations';
-import triangle from '../images/triangle.svg';
  
-import '../styles/global';
+import donwload_app_store from '../images/download_on_the_app_store_badge_us-uk_135x40.svg';
 
-const Content = styled(ParallaxLayer)`
+import '../styles/global';
+// import '../styles/beerTracker';
+
+const Content = styled.div`
   ${tw('p-6 md:p-12 lg:p-24 justify-center items-center flex z-50')};
 `;
 
@@ -18,36 +18,28 @@ const Hero = styled.div`
 `;
 
 const Title = styled.h1`
-  ${tw('text-4xl lg:text-4xl font-sans text-white mb-8 tracking-wide relative inline-block')};
+  ${tw('text-4xl font-sans text-white mb-2 tracking-wide relative inline-block')};
   text-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
-  &:before {
-    content: '';
-    width: 40px;
-    height: 40px;
-    background: url(${triangle});
-    position: absolute;
-    background-size: 40px;
-    animation: ${rotate} 4s linear infinite;
-    left: -60px;
-    top: 5px;
-  }
 `;
 
 const Subtitle = styled.p`
-  ${tw('text-2xl lg:text-4xl font-sans text-white mt-8 xxl:w-3/4')};
+  ${tw('text-4xl font-sans text-white mt-8 xxl:w-3/4')};
   text-shadow: 0 2px 15px rgba(0, 0, 0, 0.2);
+`;
+
+const DonwloadAppStore = styled.img`
+  ${tw('w-32 xl:w-48 h-auto')};
 `;
 
 const Index = () => (
   <React.Fragment>
-    <Parallax pages={1}>
-      <Content speed={0.4} offset={0}>
-        <Hero>
-            <Title>Beer Tracker</Title>
-            <Subtitle>Under construction</Subtitle>
-        </Hero>
-      </Content>
-    </Parallax>
+    <Content speed={0.4} offset={0}>
+      <Hero>
+          <Title>Beer Tracker</Title>
+          <Subtitle>Easily track how many beers (glasses, mugs, cans, bottles...) you drink.</Subtitle>
+          <DonwloadAppStore src={donwload_app_store} alt="Donwload on the App Store" /> 
+      </Hero>
+    </Content>
   </React.Fragment>
 );
 
